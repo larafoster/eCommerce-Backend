aadb3f868284348dae60268c376e1fa9b3eb18b5
lara <lara@vehicle4change.org>
2021-08-12 09:29:05 -0400

    defined relationships bwtmodels in index.js

--- models/index.js
+++ models/index.js
@@ -1,17 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require ('./Product');
+const Category = require ('./Category');
+const Tag = require ('./Tag');
+const ProductTag = require ('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+Product.belongsTo (Category, {
+  //`Product` belongs to `Category`
+  foreignKey: 'category_id', //references model category
+  onDelete: 'CASCADE', //if category is deleted so is product
+});
+Category.hasMany (Product, {
+  //`Category` has many `Product` models
+  foreignKey: 'category_id',
+});
+Product.belongsToMany (Tag, {
+  //`Product` belongs to many `Tag` models
+  through: ProductTag, //Allow products to have multiple tags and tags to have many products by using the `ProductTag` through model
+  foreignKey: 'product_id',
+});
+Tag.belongsToMany (Product, {
+  //`Tag` belongs to many `Product` models
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 module.exports = {
   Product,
   Category,
