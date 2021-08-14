aa272972a16538613c731f3c430e70407f4e48ab
lara <lara@vehicle4change.org>
2021-08-14 12:24:25 -0400

    added delete api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -26,3 +26,23 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+// DELETE a Tag
+router.delete('/:id', (req, res) => {
+  try {
+    Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No Tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
