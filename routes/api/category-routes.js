21f075db29a92dbb6ce6255619360e7b45e74bb4
lara <lara@vehicle4change.org>
2021-08-14 12:27:07 -0400

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,10 +2,22 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+/* router.get('/', (req, res) => {
+  // Get all books from the book table
+  Book.findAll().then((bookData) => {
+    res.json(bookData);
+  });
+}); 
+*/
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  // Get all books from the book table
+  Category.findAll({
+include: [Product],  // be sure to include its associated Products
+  })
+  .then((categories) => {
+    res.json(categories);
+  });
 });
 
 router.get('/:id', (req, res) => {
@@ -25,11 +37,22 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
- Category.findByIdAndUpdate(req.body)
-    .then((category) => res.status(200).json(category))
-    .catch((err) => res.status(400).json(err));
+  try {
+    Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!CategoryData[0]) {
+      res.status(404).json({ message: 'not found' });
+      return;
+    }
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
@@ -38,7 +61,7 @@ router.delete('/:id', (req, res) => {
       where: { id: req.params.id }
     });
     if (!CategoryData) {
-      res.status(404).json({ message: 'No trip with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(CategoryData);
